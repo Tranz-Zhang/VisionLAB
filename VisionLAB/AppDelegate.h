4d8d44c19201efe2f1ee99593c96effb2df16b6f
chance <tranz.zhang@gmail.com>
2017-09-22 22:00:55 +0800

    First Commit
    Setup base UI for App

---
+++ VisionLAB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VisionLAB
+//
+//  Created by chance on 22/9/2017.
+//  Copyright © 2017 Bychance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
