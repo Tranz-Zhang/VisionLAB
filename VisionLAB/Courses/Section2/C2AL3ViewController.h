62b1855e34487bc2d30143d67455479d7bc5b7af
chance <tranz.zhang@gmail.com>
2017-11-07 00:35:54 +0800

    Add C2AL3ViewController

---
+++ VisionLAB/Courses/Section2/C2AL3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  C2AL3ViewController.h
+//  VisionLAB
+//
+//  Created by chance on 7/11/2017.
+//  Copyright © 2017 Bychance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface C2AL3ViewController : UIViewController
+
+@end
---
+++ VisionLAB/Courses/Section2/C2AL3ViewController.m
@@ -0,0 +1,31 @@
+//
+//  C2AL3ViewController.m
+//  VisionLAB
+//
+//  Created by chance on 7/11/2017.
+//  Copyright © 2017 Bychance. All rights reserved.
+//
+
+#import "C2AL3ViewController.h"
+
+@interface C2AL3ViewController ()
+
+@end
+
+@implementation C2AL3ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+// Task1: correlation vs convolution
+
+// Task2: convolution property 2*W*N*N << W*W*N*N
+
+// Task3: Sharpen a image
+
+// Task4: Medain Filter
+
+@end
