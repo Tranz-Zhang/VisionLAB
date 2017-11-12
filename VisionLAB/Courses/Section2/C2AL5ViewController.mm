3f4d8e094ed01176a3dc2525ce8ac481233518a2
chance-Mac <tranz.zhang@gmail.com>
2017-11-12 18:34:09 +0800

    Finish C2AL5ViewController
    Add Laplacian Operator

--- VisionLAB/Courses/Section2/C2AL5ViewController.mm
+++ VisionLAB/Courses/Section2/C2AL5ViewController.mm
@@ -6,10 +6,13 @@
 //  Copyright © 2017 Bychance. All rights reserved.
 //
 
+#import <opencv2/opencv.hpp>
+#import <opencv2/imgcodecs/ios.h>
 #import "C2AL5ViewController.h"
 #import "VLImageMatTools.h"
 
 using namespace VLImageKit;
+using namespace cv;
 
 
 @interface GradientInfo : NSObject
@@ -36,14 +39,61 @@ using namespace VLImageKit;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.resultView.image = [UIImage imageNamed:@"simple_shapes_512.png"];
+    self.resultView.image = [UIImage imageNamed:@"Lenna_512.png"];
 }
 
+
+#pragma mark - Laplacian Operator Using OpenCV
+- (IBAction)onTestLaplacianOperator:(id)sender {
+    Mat sourceImage;
+    UIImageToMat([UIImage imageNamed:@"Lenna_512.png"], sourceImage);
+    
+    CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
+    cvtColor(sourceImage, sourceImage, CV_RGB2GRAY);
+    GaussianBlur(sourceImage, sourceImage, cv::Size(3, 3), BORDER_DEFAULT);
+    
+    // apply laplacian operator
+    Mat resultImage;
+    Laplacian(sourceImage, resultImage, CV_16S, 3, 1, 0, BORDER_DEFAULT);
+    convertScaleAbs(resultImage, resultImage);
+    
+    self.infoLabel.text = [NSString stringWithFormat:@"%.2f ms", (CFAbsoluteTimeGetCurrent() - startTime) * 1000];
+    self.resultView.image = MatToUIImage(resultImage);
+}
+
+
+#pragma mark - Sobel Operator Using OpenCV
+- (IBAction)onTestSobelOperator:(id)sender {
+    Mat sourceImage;
+    UIImageToMat([UIImage imageNamed:@"Lenna_512.png"], sourceImage);
+    
+    CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
+    cvtColor(sourceImage, sourceImage, CV_RGB2GRAY);
+    GaussianBlur(sourceImage, sourceImage, cv::Size(3, 3), BORDER_DEFAULT);
+    
+    // apply sobel operator
+    Mat grad_x, grad_y;
+    Mat abs_grad_x, abs_grad_y;
+    Mat resultImage;
+    Sobel(sourceImage, grad_x, CV_16S, 1, 0); // 得出X轴方向的边界
+    convertScaleAbs(grad_x, abs_grad_x);
+    Sobel(sourceImage, grad_y, CV_16S, 0, 1); // 得出Y轴方向的边界
+    convertScaleAbs(grad_y, abs_grad_y);
+    
+    addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, resultImage); // 中和两个方向的边界
+    
+    self.infoLabel.text = [NSString stringWithFormat:@"%.2f ms", (CFAbsoluteTimeGetCurrent() - startTime) * 1000];
+    self.resultView.image = MatToUIImage(resultImage);
+}
+
+
+
+#pragma mark - Gradient
 
 - (IBAction)onTestGradientAndMagnitude:(id)sender {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         // start gaussian correlation
-        UIImage *sourceImage = [UIImage imageNamed:@"simple_shapes_512.png"];
+        UIImage *sourceImage = [UIImage imageNamed:@"Lenna_512.png"];
         
         GrayImageMat *sourceMat = CreateGrayImageMatWithUIImage(sourceImage);
         GrayImageMat *resultMat = CreateGrayImageMatWithUIImage(self.resultView.image ?: sourceImage);
